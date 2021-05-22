2ae535970ad02a429a953af019be51ba1e1b2eda
Lauren Wenzel <Laurenzel93@aol.com>
2021-05-22 17:22:56 -0400

    added some routes

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 const path = require('path');
 
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
-const routes = require('./routes');
+const routes = require('./routes/pages');
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-app.use(logger("dev"));
-app.use(express.static("public"));
+app.use(logger('dev'));
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker", {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fitness-tracker', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
